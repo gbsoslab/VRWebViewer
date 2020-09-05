1f1f93513521849a1562ef51501b195f0e35d0db
Changbeom Choi <me@cbchoi.info>
2020-09-06 04:15:31 +0900

    vr_controller and view update

--- routes/vr.js
+++ routes/vr.js
@@ -1,18 +1,66 @@
 var express = require('express');
 var router = express.Router();
 var VRItem = require('../models/vrModel');
+var mongoose = require('mongoose');
 
 /* GET home page. */
 //router.get('/', function(req, res, next) {
 //    res.render('vr', { title: 'Express' });
 //});
 
+// init gfs
+const mongoURI = "mongodb://localhost:27017/vr_images";
+
+// connection
+const conn = mongoose.createConnection(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+let gfs;
+conn.once("open", () => {
+  // init stream
+  gfs = new mongoose.mongo.GridFSBucket(conn.db, {
+    bucketName: "images"
+  });
+});
+
+
 router.get('/:vid', function(req, res) {
     VRItem.find({region_id: req.params.vid}, function (err, vritem) {
         if (err) return res.status(500).json({ error: err });
         if (!vritem) return res.status(404).json({ error: 'vritem not found' });
-        return res.render('vr_item', { vrlist: vritem });
+
+        gfs.find({image_file:vritem.image_file}).toArray((err, files) => {
+		    // check if files
+		    if (!files || files.length === 0) {
+		      return res.render("vr_item", {
+		        files: false
+		      });
+		    } 
+	    	else 
+	    	{
+	    		console.log(files)
+			    return res.render("vr_item", { vrlist: files });
+		    }
+	    })
     })
+});
+
+router.get("/image/:filename", (req, res) => {
+  console.log('id', req.params.id)
+  const file = gfs
+    .find({
+      filename: req.params.filename
+    })
+    .toArray((err, files) => {
+      if (!files || files.length === 0) {
+        return res.status(404).json({
+          err: "no files exist"
+        });
+      }
+      gfs.openDownloadStreamByName(req.params.filename).pipe(res);
+    });
 });
 
 module.exports = router;
