e6d7392f42b0102ef83bafd6c3e9b1464f9baf7b
Changbeom Choi <me@cbchoi.info>
2020-09-18 02:47:01 +0900

    DB Structure Updated

--- models/regionModel.js
+++ models/regionModel.js
@@ -3,11 +3,7 @@ var Schema = mongoose.Schema;
 
 var regionSchema = new Schema({
     name: String,
-    location: String,
-    link_l: String,
-    link_u: String,
-    link_r: String,
-    link_d: String
+    location: String
 });
 
 module.exports = mongoose.model('region', regionSchema);
