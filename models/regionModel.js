e29fdd61f10cfd18059eef12b8bd00d0cdd7e755
Changbeom Choi <me@cbchoi.info>
2020-09-18 01:23:31 +0900

    UI and logic update

--- models/regionModel.js
+++ models/regionModel.js
@@ -4,7 +4,10 @@ var Schema = mongoose.Schema;
 var regionSchema = new Schema({
     name: String,
     location: String,
-    link: String
+    link_l: String,
+    link_u: String,
+    link_r: String,
+    link_d: String
 });
 
 module.exports = mongoose.model('region', regionSchema);
